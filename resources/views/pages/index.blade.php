51a0a4eb196c24174a96012145da9c561b526ea5
Clifford Manu <clifford@myzeepay.com>
2024-08-21 11:30:21 +0000

    favicon

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/logos/favicon.ico') }}">
     <link rel="stylesheet" href="./output.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <title>Ann & Sons Ltd</title>
