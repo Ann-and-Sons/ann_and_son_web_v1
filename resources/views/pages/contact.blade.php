c7e3c6917cc7e5039365f47e427cf4d0c157cf12
Clifford Manu <clifford@myzeepay.com>
2024-08-22 11:05:50 +0000

    new update-aug-22

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="../output.css">
     <title>Conatct Us </title>
 </head>
+
 <body>
     <!--navbar -->
     @include('widgets.navbar')
@@ -30,7 +32,8 @@
                     Our Office
                 </p>
                 <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                    For more inquiries, you can reach out to us through any of our available channels, and we will respond promptly.
+                    For more inquiries, you can reach out to us through any of our available channels, and we will
+                    respond promptly.
                 </p>
             </div>
 
@@ -118,7 +121,7 @@
                         </div>
                         <div class="ml-4">
                             <dt class="text-lg leading-6 font-medium text-gray-100">
-                                Officers Hours
+                                Office Hours
                             </dt>
                             <dd class="mt-2 text-base text-gray-400">
                                 Monday - Friday: 9am to 5pm<br>
@@ -131,86 +134,52 @@
     </div>
     <!-- footer -->
     <footer class="relative bg-black pt-8 pb-6">
-        <div
-          class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-          style="height: 80px;"
-        >
-          <svg
-            class="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
+        <div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+            style="height: 80px;">
+            <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"
+                version="1.1" viewBox="0 0 2560 100" x="0" y="0">
 
-          </svg>
+            </svg>
         </div>
         <div class="container mx-auto px-4">
-          <div class="flex flex-wrap">
-            <div class="w-full lg:w-6/12 px-4">
-              <h4 class="text-3xl text-white font-semibold">Let's keep in touch!</h4>
-              <h5 class="text-lg mt-0 mb-2 text-gray-400">
-                Find us on any of these platforms, we respond 1-2 business days.
-              </h5>
-              <div class="mt-6">
-                <button
-                  class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                  type="button"
-                >
-                  <i class="flex fab fa-twitter"></i></button
-                ><button
-                  class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                  type="button"
-                >
-                  <i class="flex fab fa-facebook-square"></i></button
-                ><button
-                  class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                  type="button"
-                >
-                  <i class="flex fab fa-dribbble"></i></button
-                ><button
-                  class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                  type="button"
-                >
-                  <i class="flex fab fa-github"></i>
-                </button>
-              </div>
-            </div>
-            <div class="w-full lg:w-6/12 px-4">
-              <div class="flex flex-wrap items-top mb-6">
-                <div class="w-full lg:w-4/12 px-4 ml-auto">
-                  <span
-                    class="block uppercase text-gray-300 text-sm font-semibold mb-2"
-                    >Useful Links</span
-                  >
-                  <ul class="list-unstyled">
-                    <li>
-                      <a
-                        class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
-                        href=""
-                        >About Us</a
-                      >
-                    </li>
-                    <li>
-                      <a
-                        class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
-                        href=""
-                        >Services</a
-                      >
-                    </li>
-                    <li>
-                      <a
-                        class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
-                        href=""
-                        >Contact Us</a
-                      >
-                    </li>
-
-                  </ul>
+            <div class="flex flex-wrap">
+                <div class="w-full lg:w-6/12 px-4">
+                    <h4 class="text-3xl text-white font-semibold">Let's keep in touch!</h4>
+                    <h5 class="text-lg mt-0 mb-2 text-gray-400">
+                        Find us on LinkedIn, we respond 1-2 business days.
+                    </h5>
+                    <a href="https://www.linkedin.com/company/ann-and-sons-limited/" target="_blank"
+                        rel="noopener noreferrer">
+                        <svg fill="#ffffff" width="50px" height="50px" viewBox="-5.5 0 32 32" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <title>linkedin</title>
+                            <path
+                                d="M0 8.219v15.563c0 1.469 1.156 2.625 2.625 2.625h15.563c0.719 0 1.406-0.344 1.844-0.781 0.469-0.469 0.781-1.063 0.781-1.844v-15.563c0-1.469-1.156-2.625-2.625-2.625h-15.563c-0.781 0-1.375 0.313-1.844 0.781-0.438 0.438-0.781 1.125-0.781 1.844zM2.813 10.281c0-1 0.813-1.875 1.813-1.875 1.031 0 1.875 0.875 1.875 1.875 0 1.031-0.844 1.844-1.875 1.844-1 0-1.813-0.813-1.813-1.844zM7.844 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.656c0.375 0 0.375 0.438 0.375 0.719 0.75-0.75 1.719-0.938 2.719-0.938 2.438 0 4 1.156 4 3.719v6.438c0 0.219-0.188 0.406-0.375 0.406h-2.75c-0.219 0-0.375-0.219-0.375-0.406v-5.813c0-0.969-0.281-1.5-1.375-1.5-1.375 0-1.719 0.906-1.719 2.125v5.188c0 0.219-0.219 0.406-0.438 0.406h-2.719c-0.156 0-0.375-0.219-0.375-0.406zM2.875 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.719c0.25 0 0.406 0.156 0.406 0.406v9.531c0 0.219-0.188 0.406-0.406 0.406h-2.719c-0.188 0-0.375-0.219-0.375-0.406z">
+                            </path>
+                        </svg>
+                    </a>
                 </div>
-                {{-- <div class="w-full lg:w-4/12 px-4">
+                <div class="w-full lg:w-6/12 px-4">
+                    <div class="flex flex-wrap items-top mb-6">
+                        <div class="w-full lg:w-4/12 px-4 ml-auto">
+                            <span class="block uppercase text-gray-300 text-sm font-semibold mb-2">Useful Links</span>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
+                                        href="">About Us</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
+                                        href="">Services</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
+                                        href="">Contact Us</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        {{-- <div class="w-full lg:w-4/12 px-4">
                   <span
                     class="block uppercase text-gray-300 text-sm font-semibold mb-2"
                     >Other Resources</span
@@ -234,26 +203,21 @@
 
                   </ul>
                 </div> --}}
-              </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <hr class="my-6 border-gray-400" />
-          <div
-            class="flex flex-wrap items-center md:justify-between justify-center"
-          >
-            <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div class="text-sm text-gray-600 font-semibold py-1">
-                Copyright © 2024 Ann and Sons
-                <a
-                  href=""
-                  class="text-gray-600 hover:text-gray-900"
-                  >Limited</a
-                >.
-              </div>
+            <hr class="my-6 border-gray-400" />
+            <div class="flex flex-wrap items-center md:justify-between justify-center">
+                <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+                    <div class="text-sm text-gray-600 font-semibold py-1">
+                        Copyright © 2024 Ann and Sons
+                        <a href="" class="text-gray-600 hover:text-gray-900">Limited</a>.
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </footer>
+    </footer>
 
 </body>
+
 </html>
--- resources/views/widgets/footer.blade.php
+++ resources/views/widgets/footer.blade.php
@@ -1,65 +1,48 @@
 <footer class="relative bg-black pt-8 pb-6">
-    <div
-      class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-      style="height: 80px;"
-    >
-      <svg
-        class="absolute bottom-0 overflow-hidden"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
-      >
-        <polygon
-          class="text-black fill-current"
-          points="2560 0 2560 100 0 100"
-        ></polygon>
-      </svg>
+    <div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style="height: 80px;">
+        <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"
+            version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+            <polygon class="text-black fill-current" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
     </div>
     <div class="container mx-auto px-4">
-      <div class="flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-4">
-          <h4 class="text-3xl text-white font-semibold">Let's keep in touch!</h4>
-          <h5 class="text-lg mt-0 mb-2 text-gray-400">
-            Find us on LinkedIn, we respond 1-2 business days.
-          </h5>
-          <a href="https://www.linkedin.com/company/ann-and-sons-limited/" target="_blank" rel="noopener noreferrer">
-          <svg fill="#ffffff" width="50px" height="50px" viewBox="-5.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <title>linkedin</title>
-            <path d="M0 8.219v15.563c0 1.469 1.156 2.625 2.625 2.625h15.563c0.719 0 1.406-0.344 1.844-0.781 0.469-0.469 0.781-1.063 0.781-1.844v-15.563c0-1.469-1.156-2.625-2.625-2.625h-15.563c-0.781 0-1.375 0.313-1.844 0.781-0.438 0.438-0.781 1.125-0.781 1.844zM2.813 10.281c0-1 0.813-1.875 1.813-1.875 1.031 0 1.875 0.875 1.875 1.875 0 1.031-0.844 1.844-1.875 1.844-1 0-1.813-0.813-1.813-1.844zM7.844 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.656c0.375 0 0.375 0.438 0.375 0.719 0.75-0.75 1.719-0.938 2.719-0.938 2.438 0 4 1.156 4 3.719v6.438c0 0.219-0.188 0.406-0.375 0.406h-2.75c-0.219 0-0.375-0.219-0.375-0.406v-5.813c0-0.969-0.281-1.5-1.375-1.5-1.375 0-1.719 0.906-1.719 2.125v5.188c0 0.219-0.219 0.406-0.438 0.406h-2.719c-0.156 0-0.375-0.219-0.375-0.406zM2.875 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.719c0.25 0 0.406 0.156 0.406 0.406v9.531c0 0.219-0.188 0.406-0.406 0.406h-2.719c-0.188 0-0.375-0.219-0.375-0.406z"></path>
-            </svg>
-            </a>
-        </div>
-        <div class="w-full lg:w-6/12 px-4">
-          <div class="flex flex-wrap items-top mb-6">
-            <div class="w-full lg:w-4/12 px-4 ml-auto">
-              <span
-                class="block uppercase text-gray-300 text-sm font-semibold mb-2"
-                >Useful Links</span
-              >
-              <ul class="list-unstyled">
-                <li>
-                  <a
-                    class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
-                    href="/pages/about"
-                    >About Us</a
-                  >
-                </li>
-                <li>
-                </li>
-                <li>
-                  <a
-                    class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
-                    href="/pages/contact"
-                    >Contact Us</a
-                  >
-                </li>
-
-              </ul>
+        <div class="flex flex-wrap">
+            <div class="w-full lg:w-6/12 px-4">
+                <h4 class="text-3xl text-white font-semibold">Let's keep in touch!</h4>
+                <h5 class="text-lg mt-0 mb-2 text-gray-400">
+                    Find us on LinkedIn, we respond 1-2 business days.
+                </h5>
+                <a href="https://www.linkedin.com/company/ann-and-sons-limited/" target="_blank"
+                    rel="noopener noreferrer">
+                    <svg fill="#ffffff" width="50px" height="50px" viewBox="-5.5 0 32 32" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>linkedin</title>
+                        <path
+                            d="M0 8.219v15.563c0 1.469 1.156 2.625 2.625 2.625h15.563c0.719 0 1.406-0.344 1.844-0.781 0.469-0.469 0.781-1.063 0.781-1.844v-15.563c0-1.469-1.156-2.625-2.625-2.625h-15.563c-0.781 0-1.375 0.313-1.844 0.781-0.438 0.438-0.781 1.125-0.781 1.844zM2.813 10.281c0-1 0.813-1.875 1.813-1.875 1.031 0 1.875 0.875 1.875 1.875 0 1.031-0.844 1.844-1.875 1.844-1 0-1.813-0.813-1.813-1.844zM7.844 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.656c0.375 0 0.375 0.438 0.375 0.719 0.75-0.75 1.719-0.938 2.719-0.938 2.438 0 4 1.156 4 3.719v6.438c0 0.219-0.188 0.406-0.375 0.406h-2.75c-0.219 0-0.375-0.219-0.375-0.406v-5.813c0-0.969-0.281-1.5-1.375-1.5-1.375 0-1.719 0.906-1.719 2.125v5.188c0 0.219-0.219 0.406-0.438 0.406h-2.719c-0.156 0-0.375-0.219-0.375-0.406zM2.875 23.125v-9.531c0-0.219 0.219-0.406 0.375-0.406h2.719c0.25 0 0.406 0.156 0.406 0.406v9.531c0 0.219-0.188 0.406-0.406 0.406h-2.719c-0.188 0-0.375-0.219-0.375-0.406z">
+                        </path>
+                    </svg>
+                </a>
             </div>
-            {{-- <div class="w-full lg:w-4/12 px-4">
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="flex flex-wrap items-top mb-6">
+                    <div class="w-full lg:w-4/12 px-4 ml-auto">
+                        <span class="block uppercase text-gray-300 text-sm font-semibold mb-2">Useful Links</span>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
+                                    href="/pages/about">About Us</a>
+                            </li>
+                            <li>
+                            </li>
+                            <li>
+                                <a class="text-gray-400 hover:text-gray-100 font-semibold block pb-2 text-sm"
+                                    href="/pages/contact">Contact Us</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    {{-- <div class="w-full lg:w-4/12 px-4">
               <span
                 class="block uppercase text-gray-300 text-sm font-semibold mb-2"
                 >Other Resources</span
@@ -83,23 +66,17 @@
 
               </ul>
             </div> --}}
-          </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <hr class="my-6 border-gray-400" />
-      <div
-        class="flex flex-wrap items-center md:justify-between justify-center"
-      >
-        <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-          <div class="text-sm text-gray-600 font-semibold py-1">
-            Copyright © 2024 Ann and Sons
-            <a
-              href=""
-              class="text-gray-600 hover:text-gray-900"
-              >Limited</a
-            >.
-          </div>
+        <hr class="my-6 border-gray-400" />
+        <div class="flex flex-wrap items-center md:justify-between justify-center">
+            <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+                <div class="text-sm text-gray-600 font-semibold py-1">
+                    Copyright © 2024 Ann and Sons
+                    <a href="" class="text-gray-600 hover:text-gray-900">Limited</a>.
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
+</footer>
